589a854d26e3c69e3035fa6933ad1ab889285f3c
Terry C <terrycetoute@gmail.com>
2020-03-23 20:12:32 -0400

    password generation works, issues of remaining characters fixed

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// create 
 
+// declare global variables
 var typeUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var typeLowercase = typeUppercase.toLowerCase();
 var typeNumeric = "0123456789"
-var typeSpecial = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~ "
+var typeSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 var userCharType = ""
+var randomPassword = ""
 
 // create a function to run the all the conditions before allowing a password to be generated
 
@@ -14,6 +15,9 @@ function passwordUserCriteriaCheck() {
   // Prompt for password criteria
   var pwCriteria = confirm("Please select password criteria");
   if (pwCriteria) {
+    // clear variables to prevent a concat of previous data is process is done repeatedly
+    randomPassword = "";
+    userCharType = "";
     // prompt for password length; min = 8, max = 128
     var pwLength = prompt("Please enter a character length between 8 and 128 characters.")
     if (pwLength >= 8 && pwLength <= 128) {
@@ -22,6 +26,8 @@ function passwordUserCriteriaCheck() {
     } else {
       alert("Invalid Entry. Please try again using a length between 8 and 128  characters.");
       var pwLengthValidation = false;
+      // call back to start process all over again
+      passwordUserCriteriaCheck();
     }
   } else {
     alert("You must click OK to proceed foward! Please click Generate Password and try again.");
@@ -29,7 +35,7 @@ function passwordUserCriteriaCheck() {
   if (pwLengthValidation == true) {
     // prompts for character types
     // lowercase, uppercase, numeric, and/or special characters
-    var pwCharType1 = prompt("Would you like 'lowercase' character types included? Type 'yes' or 'no'")
+    var pwCharType1 = prompt("Would you like 'lowercase' character types included? Type 'yes' or 'no'").trim()
     pwCharType1 = pwCharType1.toLowerCase();
     if (pwCharType1 === "yes") {
       // concat an empty string variable with proper character type to be used in the password generator
@@ -40,7 +46,7 @@ function passwordUserCriteriaCheck() {
       secondPrompt = true;
     }
     if (secondPrompt) {
-      var pwCharType2 = prompt("Would you like 'uppercase' character types included? Type 'yes' or 'no'")
+      var pwCharType2 = prompt("Would you like 'uppercase' character types included? Type 'yes' or 'no'").trim()
       pwCharType2 = pwCharType2.toLowerCase();
       if (pwCharType2 === "yes") {
         // concat an empty string variable with proper character type to be used in the password generator
@@ -52,7 +58,7 @@ function passwordUserCriteriaCheck() {
       }
     }
     if (thirdPrompt) {
-      var pwCharType3 = prompt("Would you like 'numeric' character types included? Type 'yes' or 'no'")
+      var pwCharType3 = prompt("Would you like 'numeric' character types included? Type 'yes' or 'no'").trim()
       pwCharType3 = pwCharType3.toLowerCase();
       if (pwCharType3 === "yes") {
         // concat an empty string variable with proper character type to be used in the password generator
@@ -64,7 +70,7 @@ function passwordUserCriteriaCheck() {
       }
     }
     if (fourthPrompt) {
-      var pwCharType4 = prompt("Would you like 'special characters' character types included? Type 'yes' or 'no'")
+      var pwCharType4 = prompt("Would you like 'special characters' character types included? Type 'yes' or 'no'").trim()
       pwCharType4 = pwCharType4.toLowerCase();
       if (pwCharType4 === "yes") {
         // concat an empty string variable with proper character type to be used in the password generator
@@ -80,17 +86,11 @@ function passwordUserCriteriaCheck() {
       // start all over again
       passwordUserCriteriaCheck();
     } else {
-      // console.log was left to show how I troubleshooted the code. Wanted to display how I verified my code.
-      // console.log("we are going to generate the password!")
       // all criteria is met, generate password 
 
     }
 
   }
-  // else {
-  //   // if password length validation fails
-  //   alert("Please ")
-  // }
 
 }
 
@@ -98,8 +98,9 @@ function passwordUserCriteriaCheck() {
 
 function generatePassword() {
   for (let i = 0; i < length; i++) {
-    var randomPassword = userCharType[Math.floor(Math.random() * userCharType.length)];
+    randomPassword = randomPassword + userCharType[Math.floor(Math.random() * userCharType.length)];
   }
+  return randomPassword;
 }
 
 // Write password to the #password input
@@ -113,12 +114,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// validation & required field set
-
-// generate password
-
-
-// send alert or write to page
